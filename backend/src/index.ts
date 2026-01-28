e59952e6d3fc927efc57cf2d992cd08481382117
mogars <marginean2marius@gmail.com>
2026-01-28 02:57:55 +0200

    Revert "added offices"
    This reverts commit e09ed0de9b6bb8d7fcf5fd1fc36f4c4be49464c4.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,6 @@ import treatmentTypesRouter from './routes/treatment-types';
 import appSettingsRouter from './routes/app-settings';
 import usersRouter from './routes/users';
 import smsRouter from './routes/sms';
-import officesRouter from './routes/offices';
 import { initReminderService } from './services/reminder-service';
 
 dotenv.config();
@@ -49,7 +48,6 @@ app.use('/treatment-types', treatmentTypesRouter);
 app.use('/app-settings', appSettingsRouter);
 app.use('/users', usersRouter);
 app.use('/send-sms', smsRouter);
-app.use('/offices', officesRouter);
 
 // Error handling middleware
 app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
